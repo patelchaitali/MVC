43a0643de63f78bd074405f92aff0633e154d5e4
Rakesh <rakesh@Rakeshs-MacBook-Pro.local>
2019-06-27 13:54:51 +0100

    Designing changes of navigation file.

--- app/views/nav.php
+++ app/views/nav.php
@@ -1,20 +1,20 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #f8f9fa;">
-    <div> <b><a class="navbar-brand" href="index">Student Management System</a></b></div>
-
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="/MVC/index">Dashboard <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="/MVC/create">Create</a>
-      <div class="navbar-nav" align="left">
-      <a class="nav-item nav-link" href="/MVC/login">Login</a>
-    </div>
-  </div>
-</div>
-</nav>
+<!--<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #f8f9fa;">-->
+<!--    <div> <b><a class="navbar-brand" href="index">Student Management System</a></b></div>-->
+<!---->
+<!--  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--    <span class="navbar-toggler-icon"></span>-->
+<!--  </button>-->
+<!--  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">-->
+<!---->
+<!--    <div class="navbar-nav">-->
+<!--      <a class="nav-item nav-link active" href="/MVC/index">Dashboard <span class="sr-only">(current)</span></a>-->
+<!--      <a class="nav-item nav-link" href="/MVC/create">Create</a>-->
+<!--      <div class="navbar-nav" align="left">-->
+<!--      <a class="nav-item nav-link" href="/MVC/login">Login</a>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</div>-->
+<!--</nav>-->
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <a href="index" class="navbar-brand">Student Management System</a>
@@ -24,9 +24,9 @@
 
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav">
-            <a href="#" class="nav-item nav-link active">Dashboard <span class="sr-only">(current)</span></a>
-            <a href="#" class="nav-item nav-link">About</a>
-            <a href="#" class="nav-item nav-link">Products</a>
+            <a href="/MVC/index" class="nav-item nav-link active">Dashboard <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="/MVC/create">Create</a>
+            <a href="#" class="nav-item nav-link">Login</a>
         </div>
         <form class="form-inline ml-auto">
             <input type="text" class="form-control mr-sm-2" placeholder="Search">
